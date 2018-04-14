f09567722f1273f1edf17dacc37e7cad6d9faacb
Regina Galambos <gina.galambos@gmail.com>
2018-04-14 16:58:28 +0200

    add Footer & update App.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 // import Crew from './components/crew/crew';
 
@@ -9,23 +10,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* Header */}
         <Header />
         {/* Searchbar */}
         {/* Note Manager  */}
           {/* Add new note */}
           {/* Notes View Selector */}
           {/* Notes Card Deck */}
-        {/* Footer */}
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="container">
-        <p className="btn btn-primary">my app goes here!</p>
-        </div>
-        
+        <Footer />
       </div>
     );
   }
