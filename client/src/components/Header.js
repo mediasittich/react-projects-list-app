42782a9a9fbd0b5d17c851b354a199ef58940410
Regina Galambos <gina.galambos@gmail.com>
2018-04-18 14:47:10 +0200

    finish styling header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,8 +4,11 @@ import './Header.css'
 const Header = () => (
     <header>
         <nav className="navbar navbar-custom navbar-expand-lg navbar-dark teal lighten-2 scrolling-navbar">
-            <span className="navbar-brand navbar-brand-custom" href="#"><i className="fa fa-inbox fa-2x" aria-hidden="true"></i><strong> <h1>ProjectBox</h1></strong></span>
-            <ul className="navbar-nav nav-flex-icons ml-auto">
+            <span className="navbar-brand navbar-brand-custom" href="#">
+                <i className="fa fa-inbox fa-2x" aria-hidden="true"></i>
+                <h1>ProjectBox</h1>
+            </span>
+            <ul className="navbar-nav nav-flex-icons ml-auto mr-1">
                 <li className="nav-item">
                     <a className="nav-link" href="https://github.com/mediasittich/react-projects-list-app"><i className="fa fa-github fa-2x"></i></a>
                 </li>
