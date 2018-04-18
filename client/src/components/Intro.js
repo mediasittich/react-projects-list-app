5f65df4426a54e503a242bfab339c6bd7e654f5b
Regina Galambos <gina.galambos@gmail.com>
2018-04-18 22:48:30 +0200

    add components for auth handling

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -9,10 +9,16 @@ class Intro extends Component {
             <div className="view d-flex justify-content-center align-items-center teal lighten-2">
                 <div className="container">
                     <div className="text-center text-white">
-                    <i className="fa fa-inbox fa-4x" aria-hidden="true"></i>
-                    <h1>ProjectBox</h1>
-                        <button className="btn"><Link to="/signup">Sign Up</Link></button>
-                        <button className="btn"><Link to="/login">Login</Link></button>
+                        <section className="mb-3">
+                            <i className="fa fa-inbox fa-4x" aria-hidden="true"></i>
+                            <h1 style={{display: 'inline-block', fontSize: '3.8rem'}}>ProjectBox</h1>
+                        </section>
+                        
+                        <section className="mt-3">
+                            <Link className="intro-btn" to="/signup">Sign Up</Link>
+                            <Link className="intro-btn" to="/login">Login</Link>
+                        </section>
+                        
                     </div>
                 </div>
             </div>
